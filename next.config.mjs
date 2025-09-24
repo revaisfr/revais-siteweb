8dc3d4afb7f771d9be8a195b25cdde663f713fb7
revaisfr <contact@revais.fr>
2025-09-24 10:24:21 +0000

    finish

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['nodemailer'],
-  },
-  // Forcer la résolution des dépendances
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react': require.resolve('react'),
-      'react-dom': require.resolve('react-dom'),
-    }
-    return config
-  },
 }
 
 export default nextConfig
